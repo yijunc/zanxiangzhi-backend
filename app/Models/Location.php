d99790fd8c6af82998b3c37be61098a1c3db353d
BenjaminCHEN2016 <BenjaminCHEN2016@outlook.com>
2018-05-20 16:04:54 +0800

    location🤔🤔🤔

---
+++ app/Models/Location.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ubuntu
+ * Date: 5/20/18
+ * Time: 1:51 PM
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+
+}
