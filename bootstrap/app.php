98d9676edf830f395218754944f8596e52e75850
BenjaminCHEN2016 <BenjaminCHEN2016@outlook.com>
2018-05-24 11:38:26 +0800

    🚲🚲🚲

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -94,6 +94,7 @@ $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(\Hhxsv5\LaravelS\Illuminate\LaravelSServiceProvider::class);
 $app->register(\Illuminate\Redis\RedisServiceProvider::class);
+$app->register(\Junning\Sdk\usr\MQTTService::class);
 
 /*
 |--------------------------------------------------------------------------
---
+++ extent/Junning/Sdk/usr/MQTTService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ubuntu
+ * Date: 5/23/18
+ * Time: 11:28 PM
+ */
+
+namespace Junning\Sdk\usr;
+
+
+class MQTTService
+{
+
+}
